597b660ee69e61db49510662fe87151b31353ced
Alden Fachetti <aldenmerlin@gmail.com>
2021-06-13 02:38:47 -0300

    Eslint, Prettier and Editorconfig included

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,53 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const isProd = process.env.NODE_ENV !== 'production'
 
-module.exports = {
-  mode: "none",
-  entry: "./src/index.js",
-  output: {
-    path: __dirname + "/dist",
-    filename: "bundle.js",
-  },
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-  },
+const config = {
+  mode: 'development',
+  devtool: isProd ? 'source-map' : 'eval-cheap-module-source-map',
+  entry: './src/index.js',
   module: {
     rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
+            presets: ['@babel/preset-env']
+          }
+        }
       },
-    ],
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: 'eslint-loader'
+      //   }
+      // },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
-  plugins: [new HtmlWebpackPlugin({ title: "Challenge Front-End Easynvest" })],
-};
+  devServer: {
+    inline: false,
+    contentBase: path.join(__dirname, 'dist'),
+    port: 8000,
+    hot: true
+  },
+  output: {
+    publicPath: '/',
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      title: 'Challenge Front-End Easynvest'
+    })
+  ]
+}
+
+module.exports = config
